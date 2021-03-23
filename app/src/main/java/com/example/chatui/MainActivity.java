025ff293dc57840db5f301d51af49a601b57f4d6
Wizard <ozx1341530199@gmail.com>
2021-03-23 22:36:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chatui/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.chatui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private List<Msg> msgList = new ArrayList<>();
+    private List<Msg> cacheMsgList = new ArrayList<>();
+    private EditText inputText;
+    private Button sendButton;
+    private RecyclerView msgRecyclerView;
+    private MsgAdapter adapter;
+    private NetWorkChangeReciever netWorkChangeReciever;
+    private MydataBase mydataBase;
+    private SQLiteDatabase db;
+    private boolean chatChange = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initMsg();
+        mydataBase = new MydataBase(this, "ChatUI.db", null, 1);
+        this.db = mydataBase.getWritableDatabase();
+        inputText = (EditText) findViewById(R.id.input_text);
+        sendButton = (Button) findViewById(R.id.send_button);
+        msgRecyclerView = (RecyclerView) findViewById(R.id.msg_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        msgRecyclerView.setLayoutManager(layoutManager);
+        msgList = readCache();
+        adapter = new MsgAdapter(msgList);
+        msgRecyclerView.setAdapter(adapter);
+//        IntentFilter intentFilter = new IntentFilter();
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        netWorkChangeReciever = new NetWorkChangeReciever();
+//        registerReceiver(netWorkChangeReciever, intentFilter);
+        inputText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    inputText.setHint(null);
+                    msgRecyclerView.scrollToPosition(msgList.size()-1);
+                }
+            }
+        });
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = inputText.getText().toString();
+                if (!Objects.equals("", content)) {
+                    Msg msg = new Msg(content, Msg.TYPE_SENT);
+                    msgList.add(msg);
+                    cacheMsgList.add(msg);
+                    adapter.notifyItemInserted(msgList.size()-1);
+                    msgRecyclerView.scrollToPosition(msgList.size()-1);
+                    inputText.setText("");
+
+                }
+                for (Msg x : msgList) {
+                    System.out.println(x.getContent());
+                }
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cacheToStorage();
+
+           //会造成多次缓存相同内容的bug，待解决
+        //unregisterReceiver(netWorkChangeReciever);
+    }
+
+    private void initMsg() {
+        Msg msg1 = new Msg("渣男狗都不吃", Msg.TYPE_RECIEVED);
+        msgList.add(msg1);
+        Msg msg2 = new Msg("@只信他公 @只信他爹 ", Msg.TYPE_RECIEVED);
+        msgList.add(msg2);
+        Msg msg3 = new Msg("狗才不吃", Msg.TYPE_RECIEVED);
+        msgList.add(msg3);
+        Msg msg4= new Msg("sbg ljg", Msg.TYPE_SENT);
+        msgList.add(msg4);
+    }
+
+
+
+    private void cacheToStorage() {
+        for(Msg x: this.cacheMsgList) {
+            String content = x.getContent();
+            int contentType = x.getType();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MydataBase.getColumnChatContent(), content);
+            contentValues.put(MydataBase.getColumnChatType(), contentType);
+            this.db.insert(MydataBase.getTableName(), null, contentValues);
+            contentValues.clear();
+        }
+    }
+
+    private List<Msg> readCache() {
+
+        List<Msg> list = new ArrayList<>();
+        Cursor cursor = this.db.query(MydataBase.getTableName(), null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                String content = cursor.getString(cursor.getColumnIndex(MydataBase.getColumnChatContent()));
+                int contentType = cursor.getInt(cursor.getColumnIndex(MydataBase.getColumnChatType()));
+                Msg msg = new Msg(content, contentType);
+                list.add(msg);
+            }while(cursor.moveToNext());
+        }
+
+        return list;
+    }
+
+    class NetWorkChangeReciever extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "检测到网络状态变化", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/chatui/MsgAdapter.java
@@ -0,0 +1,63 @@
+package com.example.chatui;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.chatui.R;
+
+import java.util.*;
+
+public class MsgAdapter extends RecyclerView.Adapter<MsgAdapter.ViewHolder> {
+    private List<Msg> msgList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout leftLayout;
+        LinearLayout rightLayout;
+        TextView leftMsg;
+        TextView rightMsg;
+
+        public ViewHolder(View view) {
+            super(view);
+            this.leftLayout = (LinearLayout) view.findViewById(R.id.left_layout);
+            this.rightLayout = (LinearLayout) view.findViewById(R.id.right_layout);
+            this.leftMsg = (TextView) view.findViewById(R.id.left_msg);
+            this.rightMsg = (TextView) view.findViewById(R.id.right_msg);
+        }
+    }
+
+    public MsgAdapter(List<Msg> msgList) {
+        this.msgList = msgList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.msg_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Msg msg = msgList.get(position);
+        if (msg.getType() == Msg.TYPE_RECIEVED) {
+            holder.leftLayout.setVisibility(View.VISIBLE);
+            holder.rightLayout.setVisibility(View.GONE);
+            holder.leftMsg.setText(msg.getContent());
+        } else {
+            holder.rightLayout.setVisibility(View.VISIBLE);
+            holder.leftLayout.setVisibility(View.GONE);
+            holder.rightMsg.setText(msg.getContent());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return msgList.size();
+        
+    }
+}
